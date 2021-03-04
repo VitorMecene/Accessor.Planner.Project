a69fff552dc187f56d1c88448f8806169c578dc7
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2021-03-04 20:02:18 -0300

    Added Entity Framework

--- Accessor.Planner/src/Server/Services/Accessor.Planner.Domain/Model/Commom/DefaultValues.cs
+++ Accessor.Planner/src/Server/Services/Accessor.Planner.Domain/Model/Commom/DefaultValues.cs
@@ -9,7 +9,7 @@ namespace Accessor.Planner.Domain.Model.Commom
         public T Id { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
         public DateTime UpdatedAt { get; protected set; }
-        public DateTime DeletedAt { get; protected set; }
+        public DateTime? DeletedAt { get; protected set; }
         public bool Activate { get; protected set; }
     }
 }
---
+++ Accessor.Planner/src/Server/Services/Accessor.Planner.Infrastructure/Data/ApplicationDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Accessor.Planner.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accessor.Planner.Infrastructure.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Solicitation> Solicitations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
